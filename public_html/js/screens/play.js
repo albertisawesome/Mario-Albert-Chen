33dc1e146449b4924f31c2d45e00946f168d619f
Albert Chen <chenalbert60@gmail.com>
2014-11-14 15:15:12 -0800

    done with video 7

--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -7,7 +7,12 @@ game.PlayScreen = me.ScreenObject.extend({
 		game.data.score = 0;
                 
                 me.levelDirector.loadLevel("World 1");
-
+                
+                 var player = me.pool.pull("mario", 0, 420, {});
+                 me.game.world.addChild(player, 3);
+                 
+                 me.input.bindKey(me.input.KEY.RIGHT, "right");
+                 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
 		me.game.world.addChild(this.HUD);
